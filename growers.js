6e2b785c97497210710e71b930af6cb1ae14e5b6
Alloatti <guidoalloatti@gmail.com>
2019-05-15 02:38:30 -0300

    Small refactor

--- growers.js
+++ growers.js
@@ -1,95 +1,3 @@
-var growers = [
-	{
-		name: "Farmer 1",
-		farmsOwned: "2",
-		farms: [
-			{
-				plantingEvents: [
-					{
-						date: "07/07/2019",
-						cropType: "corn"
-					},
-					{
-						date: "12/12/2019",
-						cropType: "soybeans"
-					}
-				]
-			},
-			{
-				plantingEvents: [
-					{
-						date: "06/06/2017",
-						cropType: "corn"
-					},
-					{
-						date: "10/10/2018",
-						cropType: "soybeans"
-					}
-				]
-			}
-		]
-	},
-	{
-		name: "Farmer 2",
-		farmsOwned: "1",
-		farms: [
-			{
-				plantingEvents: [
-					{
-						date: "08/08/2019",
-						cropType: "corn"
-					},
-					{
-						date: "12/12/2018",
-						cropType: "soybeans"
-					}
-				]
-			}
-		]
-	},
-	{
-		name: "Farmer 3",
-		farmsOwned: "3",
-		farms: [
-			{
-				plantingEvents: [
-					{
-						date: "09/09/2017",
-						cropType: "corn"
-					},
-					{
-						date: "12/10/2017",
-						cropType: "soybeans"
-					}
-				]
-			},
-			{
-				plantingEvents: [
-					{
-						date: "09/06/2019",
-						cropType: "corn"
-					},
-					{
-						date: "12/10/2018",
-						cropType: "soybeans"
-					}
-				]
-			},
-			{
-				plantingEvents: [
-					{
-						date: "06/07/2017",
-						cropType: "corn"
-					},
-					{
-						date: "11/10/2019",
-						cropType: "soybeans"
-					}
-				]
-			}
-		]
-	}
-]
 var growersList = [];
 var growersDetails = [];
 var testsResults = [];
@@ -98,15 +6,12 @@ function getGrowerNamesThisYear(plantation) {
 	growersList = [];
 	growersDetails = [];
 	var thisYear = new Date().getFullYear();
-	var i;
-	var j;
-	var h;
 
-	for (i = 0; i < growers.length; i++) {
+	for (var i = 0; i < growers.length; i++) {
 	  var grower = growers[i];
-	  for (j = 0; j < grower.farms.length; j++) {
+	  for (var j = 0; j < grower.farms.length; j++) {
 	  	var farm = grower.farms[j];
-	  	for (h = 0; h < farm.plantingEvents.length; h++ ) {
+	  	for (var h = 0; h < farm.plantingEvents.length; h++ ) {
 	  		var planting = farm.plantingEvents[h];
 	  		var plantingDate = new Date(planting.date);
 	  		var plantingYear = plantingDate.getFullYear();
@@ -115,9 +20,7 @@ function getGrowerNamesThisYear(plantation) {
 	  			growersList.push(grower.name);
 	  		}
 	  	}
-	  	h=0;
 	  }
-	  j=0;
 	}
 }
 
@@ -133,11 +36,9 @@ function showGrowerNamesByPlantation(plantation) {
 	getGrowerNamesThisYear(plantation);
 	var i;
 	var html = "<ul>";
-
 	for (i = 0; i < growersList.length; i++)	{
 		html = html + "<li>" + parseInt(i+1) + "). " + growersList[i] + "</li>";
 	}
-
 	html = html + "</ul>";
 	document.getElementById("grower_names_" + plantation).innerHTML = html;
 }
@@ -147,11 +48,9 @@ function showGrowerNamesByPlantationRecentFirst(plantation) {
 	var growers = sortGrowersByRecentPlantation();
 	var i;
 	var html = "<ul>";
-
 	for (i = 0; i < growers.length; i++)	{
 		html = html + "<li>" + parseInt(i+1) + "). " + growers[i].grower_name + " planted " + growers[i].planted + " on " + growers[i].date.toLocaleDateString() + "</li>";
 	}
-
 	html = html + "</ul>";
 	document.getElementById("grower_names_by_recent_" + plantation).innerHTML = html;
 }
---
+++ growers_json.js
@@ -0,0 +1,92 @@
+var growers = [
+	{
+		name: "Farmer 1",
+		farmsOwned: "2",
+		farms: [
+			{
+				plantingEvents: [
+					{
+						date: "07/07/2019",
+						cropType: "corn"
+					},
+					{
+						date: "12/12/2019",
+						cropType: "soybeans"
+					}
+				]
+			},
+			{
+				plantingEvents: [
+					{
+						date: "06/06/2017",
+						cropType: "corn"
+					},
+					{
+						date: "10/10/2018",
+						cropType: "soybeans"
+					}
+				]
+			}
+		]
+	},
+	{
+		name: "Farmer 2",
+		farmsOwned: "1",
+		farms: [
+			{
+				plantingEvents: [
+					{
+						date: "08/08/2019",
+						cropType: "corn"
+					},
+					{
+						date: "12/12/2018",
+						cropType: "soybeans"
+					}
+				]
+			}
+		]
+	},
+	{
+		name: "Farmer 3",
+		farmsOwned: "3",
+		farms: [
+			{
+				plantingEvents: [
+					{
+						date: "09/09/2017",
+						cropType: "corn"
+					},
+					{
+						date: "12/10/2017",
+						cropType: "soybeans"
+					}
+				]
+			},
+			{
+				plantingEvents: [
+					{
+						date: "09/06/2019",
+						cropType: "corn"
+					},
+					{
+						date: "12/10/2018",
+						cropType: "soybeans"
+					}
+				]
+			},
+			{
+				plantingEvents: [
+					{
+						date: "06/07/2017",
+						cropType: "corn"
+					},
+					{
+						date: "11/10/2019",
+						cropType: "soybeans"
+					}
+				]
+			}
+		]
+	}
+]
